380ce00b10e3c8850a3893a322daff6e0ae3f403
Merritt Boyd <mboyd@mit.edu>
2011-02-11 02:03:07 -0500

    Theme changes.

---
+++ ledweb/public/javascripts/bemis100_canvas.js
@@ -0,0 +1,43 @@
+var canvas;
+var ctx;
+var ws;
+
+window.onload = function() {
+  canvas = document.getElementById('canvas');
+  ctx = canvas.getContext('2d');
+  
+  startSocketCheck();
+};
+
+function connect() {
+  ws = new WebSocket("ws://localhost:9999");
+  
+  ws.onmessage = function(e) {
+    var data = JSON.parse(e.data);
+    
+    if (data['status'] == 'ok') {
+      var frame = data['frame'];
+      var pixelWidth = canvas.width / (83 * 2);
+      var height = canvas.height;
+      
+      for (var i = 0; i < frame.length; i++) {
+        ctx.fillStyle = frame[i];
+        ctx.fillRect(i*pixelWidth, 0, pixelWidth, height);
+      }
+    } else if (data['status'] == 'exiting') {
+      ws.close();
+    }
+  };
+  
+  ws.onclose = function() {
+    setTimeout(startSocketCheck, 1000);
+  }
+}
+
+function startSocketCheck() {
+  try {
+    connect();
+  } catch (err) {
+    setTimeout(startSocketCheck, 500);
+  }
+}
---
+++ ledweb/templates/Bemis100.mako
@@ -0,0 +1,21 @@
+<%inherit file="/base.mako"/>
+
+
+
+<div id='queue'>
+	<h2>Queue</h2>
+	<p id='controls'>
+		<a>Play</a>
+	</p>
+	<ul>
+		<li>Pattern 1</li>
+  	<li>Pattern 2</li>
+	</ul>
+</div>
+
+<div id='patterns'>
+	<h2>Patterns</h2>
+	% for p in c.patterns:
+		<img class='pattern' alt='pattern' src='${c.pattern_dir}/${p}'>
+	% endfor
+</div>
