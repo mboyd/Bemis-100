63d6747a3f9a94350fc974cc710585784238aa2e
rdeits <robin.deits@gmail.com>
2011-02-12 02:20:18 -0500

    began adding support for beat tracking with websocket graphics

--- ledweb/ledweb/controllers/Bemis100.py
+++ ledweb/ledweb/controllers/Bemis100.py
@@ -9,6 +9,7 @@ from ledweb.lib.base import BaseController, render
 import os, os.path
 
 from ledweb.lib.led.pattern import *
+from ledweb.lib.led.beat import *
 
 log = logging.getLogger(__name__)
 
@@ -39,6 +40,27 @@ class Bemis100Controller(BaseController):
         
         app_globals.bemis100.play()
         return dict(success=True)
+        elif request.params.has_key('beatpattern'):
+            try:
+                pattern_file =
+                os.path.join('ledweb/public/',config['pattern_dir'],\
+                        request.params['beatpattern'])
+                base = Bemis100Pattern(pattern_file, int(config['num_boards']))
+                p = RMSBeatPattern
+                if request.params.has_key('num_times'):
+                    n = request.params['num_times']
+                else:
+                    n = -1
+                
+                app_globals.bemis100.add_pattern(p, n)
+            
+            except Exception, e:
+                return dict(success=False, error=str(e))
+        
+        app_globals.bemis100.play()
+        return dict(success=True)
+
+
     
     @jsonify
     def queue(self):
