85ebfcead7130619efc87958f61af18f43be675a
Merritt Boyd <mboyd@mit.edu>
2011-02-18 18:29:25 -0500

    Fix webapp connection state determination, merge bemis100_canvas.js and bemis100.js

--- Web/static/javascripts/bemis100.js
+++ Web/static/javascripts/bemis100.js
@@ -1,5 +1,20 @@
+var canvas;
+var ctx;
+var ws;
+
+var frameCount;
+var lastTime;
+
 $(document).ready(function() {
-  updateQueue();
+  
+  canvas = document.getElementById('canvas');
+  ctx = canvas.getContext('2d');
+  blank();
+  
+  frameCount = 0;
+  lastTime = (new Date).getTime();
+  
+  connect();
   
   $('#controls a').click(function(evt) {
     evt.preventDefault();
@@ -39,10 +54,66 @@ $(document).ready(function() {
   
 });
 
+function connect() {
+  ws = new WebSocket("ws://localhost:9999");
+  
+  ws.onopen = function() {
+    $('#connection').html('Status: connected');
+    updateQueue();
+  }
+  
+  ws.onmessage = function(e) {
+    var data = JSON.parse(e.data);
+    
+    if (data['status'] == 'ok') {
+      var frame = data['frame'];
+      var pixelWidth = canvas.width / frame.length;
+      var height = canvas.height;
+      
+      for (var i = 0; i < frame.length; i++) {
+        ctx.fillStyle = frame[i];
+        ctx.fillRect(i*pixelWidth, 0, pixelWidth, height);
+      }
+      
+      if (frameCount == 30) {
+        frameCount = 0;
+        var t = (new Date).getTime();
+        var dt = t - lastTime;
+        var fr = 30 / (dt / 1000.0);
+        $('#framerate').html('Framerate: ' + fr.toFixed(2) + ' fps');
+      
+        lastTime = t;
+      }
+      
+      frameCount += 1;
+      
+    } else if (data['status'] == 'exiting') {
+      $('#framerate').html('');
+      ws.close();
+    }
+  };
+  
+  ws.onclose = function() {
+    $('#connection').html('Status: disconnected');
+    blank();
+    setTimeout(connect, 2000);
+  }
+}
+
+function blank() {
+  ctx.fillStyle = 'rgb(0,0,0)';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
 function updateQueue() {
   $.getJSON('/queue', function(result) {
     var queue = result['queue'];
-    var qh = ''
+    var qh = '';
+    
+    if (queue.length == 0) {
+      qh = '<li>Queue empty</li>';
+    }
+    
     for (var i = 0; i < queue.length; i++) {
       var p = queue[i][0];
       var n = queue[i][1];
