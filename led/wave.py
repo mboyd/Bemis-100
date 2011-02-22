55da354e04534004effbd7aeeab6ac4cbdd8a09a
rdeits <robin.deits@gmail.com>
2011-02-22 04:03:00 -0500

    Big performance improvements to wave.py.

--- led/wave.py
+++ led/wave.py
@@ -18,15 +18,23 @@ class WavePattern:
         self.out = np.zeros(self.pixels*3)
 
     def get_line(self):
-        for i in range(1,self.pixels-2):
-            self.acc[i] = ((self.pos[i+1]-2*self.pos[i]+\
-                    self.pos[i-1])*T-friction*self.vel[i])/mu;
+        # for i in range(1,self.pixels-2):
+            # self.acc[i] = ((self.pos[i+1]-2*self.pos[i]+\
+                    # self.pos[i-1])*T-friction*self.vel[i])/mu;
 
-        for i in range(1,self.pixels-2):
-            self.vel[i] = (self.vel[i] + self.acc[i]*dt);
+        # for i in range(1,self.pixels-2):
+            # self.vel[i] = (self.vel[i] + self.acc[i]*dt);
 
-        for i in range(1,self.pixels-2):
-            self.pos[i] = self.pos[i] + self.vel[i]*dt;
+        # for i in range(1,self.pixels-2):
+            # self.pos[i] = self.pos[i] + self.vel[i]*dt;
+
+        self.acc[1:self.pixels-1] = ((self.pos[2:self.pixels]-\
+                2*self.pos[1:self.pixels-1]+\
+                self.pos[0:self.pixels-2])*T-friction*self.vel[1:self.pixels-1])/mu
+        
+        self.vel[1:self.pixels-1] += self.acc[1:self.pixels-1]*dt
+
+        self.pos[1:self.pixels-1] += self.vel[1:self.pixels-1]*dt
 
         for i in range(self.pixels):
             # new_point = abs(self.data[i])
@@ -36,17 +44,24 @@ class WavePattern:
             elif self.new_point < -1:
                 self.new_point = -1
             if self.new_point > 0:
+                # self.new_data = np.array([0,0,self.new_point*255])
+                # self.new_data = [0,0,self.new_point*255]
                 self.out[i*3] = 0
                 self.out[i*3+1] = 0
                 self.out[i*3+2] = self.new_point*255
             elif self.new_point < 0:
+                # self.new_data = np.array([-self.new_point*255,0,0])
+                # self.new_data = [-self.new_point*255,0,0]
                 self.out[i*3] = -self.new_point*255
                 self.out[i*3+1] = 0
                 self.out[i*3+2] = 0
             else:
+                # self.new_data = np.zeros(3)
+                # self.new_data = [0,0,0]
                 self.out[i*3] = 0
                 self.out[i*3+1] = 0
                 self.out[i*3+2] = 0
+            # self.out[i*3:i*3+3] = self.new_data
         return bytearray((pattern.encode_char(c) for c in self.out))
 
     def __iter__(self):
