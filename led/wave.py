3cfb8d269c3ccb6468def63caffa8a513b4c995f
rdeits <robin.deits@gmail.com>
2011-02-21 19:26:38 -0500

    Wave pattern no longer generated in a separate thread.

--- led/wave.py
+++ led/wave.py
@@ -4,51 +4,36 @@ from multiprocessing import Process, Pipe
 import pattern
 import numpy as np
 
-def make_wave(pixels,input_parent,data_child):
-    pos = np.zeros(pixels)
-    vel = np.zeros(pixels)
-    acc = np.zeros(pixels)
-    T = .1; # "Tension" 
-    mu = 1; # "mass per length"
-    friction = 0; # frictional force per velocity
-    dt = .1;
-    while True:
-        while input_parent.poll():
-            pos += input_parent.recv()
-        pos[0] = 0
-        pos[len(pos)-1] = 0
-        for i in range(1,pixels-2):
-            acc[i] = ((pos[i+1]-2*pos[i]+pos[i-1])*T-friction*vel[i])/mu;
-
-        for i in range(1,pixels-2):
-            vel[i] = (vel[i] + acc[i]*dt);
-
-        for i in range(1,pixels-2):
-            pos[i] = pos[i] + vel[i]*dt;
-        data_child.send(pos)
+T = .1; # "Tension" 
+mu = 1; # "mass per length"
+friction = 0; # frictional force per velocity
+dt = .1;
 
 class WavePattern:
     def __init__(self,num_boards = 83):
-        self.num_boards = num_boards
-        self.data = np.zeros(num_boards*2)
-        
-    def start_wave(self):
-        print "starting wave"
-        self.data_parent, data_child = Pipe(duplex=False)
-        input_parent, self.input_child = Pipe(duplex=False)
-        
-        proc = Process(target = make_wave,
-                args=(self.num_boards*2,input_parent,data_child))
-        proc.daemonic = True
-        proc.start()
+        self.pixels = num_boards*2
+        self.pos = np.zeros(self.pixels)
+        self.vel = np.zeros(self.pixels)
+        self.acc = np.zeros(self.pixels)
 
     def get_line(self):
-        while self.data_parent.poll():
-            self.data = self.data_parent.recv()
-        out = np.zeros((self.num_boards*2,3))
+        self.pos[0] = 0
+        self.pos[len(self.pos)-1] = 0
+        
+        for i in range(1,self.pixels-2):
+            self.acc[i] = ((self.pos[i+1]-2*self.pos[i]+\
+                    self.pos[i-1])*T-friction*self.vel[i])/mu;
+
+        for i in range(1,self.pixels-2):
+            self.vel[i] = (self.vel[i] + self.acc[i]*dt);
+
+        for i in range(1,self.pixels-2):
+            self.pos[i] = self.pos[i] + self.vel[i]*dt;
+
+        out = np.zeros((self.pixels,3))
         for i in range(len(out)):
             # new_point = abs(self.data[i])
-            new_point = self.data[i]
+            new_point = self.pos[i]
             if new_point > 1:
                 new_point = 1
             if new_point < -1:
@@ -61,19 +46,18 @@ class WavePattern:
         return bytearray([pattern.encode_char(c) for c in out])
 
     def __iter__(self):
-        self.start_wave()
         start_data = np.array(\
                 [0]+\
-                [0]*(self.num_boards-11)+\
+                [0]*np.floor(self.pixels/2-11)+\
                 [1]*20+\
-                [0]*(self.num_boards-11)+\
+                [0]*np.ceil(self.pixels/2-11)+\
                 [0])
 
         # start_data = np.zeros(self.num_boards*2)
         # for i in range(len(start_data)):
             # start_data[i] = np.sin(i*np.pi*2/(self.num_boards*2-1))
 
-        self.input_child.send(start_data)
+        self.pos += start_data
         return iter(self.get_line,None)
 
     
