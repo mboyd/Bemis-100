2e69d613c0b1f74592923adc2d93a4d3120551fb
Robin Deits <robin.deits@gmail.com>
2011-10-20 17:14:41 -0400

    Refactoring of wave pattern for cleanup

--- led/wave.py
+++ led/wave.py
@@ -20,16 +20,17 @@ class WavePattern:
         self.out = np.zeros(self.pixels*3)
 
     def get_line(self):
-        # for i in range(1,self.pixels-2):
-            # self.acc[i] = ((self.pos[i+1]-2*self.pos[i]+\
-                    # self.pos[i-1])*T-friction*self.vel[i])/mu;
+        self.update_physics()
 
-        # for i in range(1,self.pixels-2):
-            # self.vel[i] = (self.vel[i] + self.acc[i]*dt);
+        if random.random() < .02:
+            self.add_pulse()
 
-        # for i in range(1,self.pixels-2):
-            # self.pos[i] = self.pos[i] + self.vel[i]*dt;
+        for i in range(self.pixels):
+            self.out[i*3:i*3+3] = self.output_func(self.pos[i])
 
+        return bytearray((int(i) for i in self.out))
+
+    def update_physics(self):
         self.acc[1:self.pixels-1] = ((self.pos[2:self.pixels]-\
                 2*self.pos[1:self.pixels-1]+\
                 self.pos[0:self.pixels-2])*T-friction*self.vel[1:self.pixels-1])/mu
@@ -40,43 +41,29 @@ class WavePattern:
 
         self.pos[1:self.pixels-1] += self.vel[1:self.pixels-1]*dt
 
-        if random.random() < .02:
-            pulse_center = random.randrange(0, self.pixels)
-            pulse_width = random.randrange(1, 15)
-            if random.random() < .5:
-                pulse_sign = -1
-            else:
-                pulse_sign = 1
-            self.pos[max(1, pulse_center - pulse_width//2):min(self.pixels-1, 
-                                                               pulse_center + pulse_width//2)] = pulse_sign
+    def output_func(self, point):
+        if point > 1:
+            point = 1
+        elif point < -1:
+            point = -1
+        if point > 0:
+            out = [0, 0, int(point*255)]
+        elif point < 0:
+            out = [int(-point*255), 0, 0]
+        else:
+            out = [0, 0, 0]
+        return out
 
-        for i in range(self.pixels):
-            # new_point = abs(self.data[i])
-            self.new_point = self.pos[i]
-            if self.new_point > 1:
-                self.new_point = 1
-            elif self.new_point < -1:
-                self.new_point = -1
-            if self.new_point > 0:
-                # self.new_data = np.array([0,0,self.new_point*255])
-                # self.new_data = [0,0,self.new_point*255]
-                self.out[i*3] = 0
-                self.out[i*3+1] = 0
-                self.out[i*3+2] = int(self.new_point*255)
-            elif self.new_point < 0:
-                # self.new_data = np.array([-self.new_point*255,0,0])
-                # self.new_data = [-self.new_point*255,0,0]
-                self.out[i*3] = int(-self.new_point*255)
-                self.out[i*3+1] = 0
-                self.out[i*3+2] = 0
-            else:
-                # self.new_data = np.zeros(3)
-                # self.new_data = [0,0,0]
-                self.out[i*3] = 0
-                self.out[i*3+1] = 0
-                self.out[i*3+2] = 0
-            # self.out[i*3:i*3+3] = self.new_data
-        return bytearray((int(i) for i in self.out))
+
+    def add_pulse(self):
+        pulse_center = random.randrange(0, self.pixels)
+        pulse_width = random.randrange(1, 15)
+        if random.random() < .5:
+            pulse_sign = -1
+        else:
+            pulse_sign = 1
+        self.pos[max(1, pulse_center - pulse_width//2):min(self.pixels-1, 
+                                                           pulse_center + pulse_width//2)] = pulse_sign
 
     def __iter__(self):
         pulse_width = 20
