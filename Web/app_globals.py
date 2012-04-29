86ffd8952397571cf7380d12d203035f523eb57f
Robin Deits <robin.deits@gmail.com>
2012-04-29 15:56:12 -0400

    Separated out the bemis100-specific config

--- Web/app_globals.py
+++ Web/app_globals.py
@@ -4,14 +4,13 @@
 from led.bemis100 import Bemis100
 from led.ge import GEController
 
-config = {'pattern_dir' : 'static/patterns',
-               # 'device' : 'sim',
-               'device' : '/dev/tty.usbserial-A400gmfr',
-           'num_lights' : 166,
-            'framerate' : 40 }
+config = {'pattern_dir' : 'static/patterns'}
 
-bemis100 = Bemis100(config['device'], num_boards=83, \
-         framerate=config['framerate'])
+bemis100_config = {'device': '/dev/tty.usbserial-A400gmfr',
+                   'num_boards': 83,
+                   'framerate': 40}
+
+bemis100 = Bemis100(**bemis100_config)
 #bemis100 = GEController(device=config['device'],
 #                        framerate=config['framerate'],
 #                        num_lights=config['num_lights'])
