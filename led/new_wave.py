b85c6e9127096a411c32aa4600222a749c5fc8b0
Robin Deits <robin.deits@gmail.com>
2012-09-29 21:02:38 -0400

    made new_wave on a red-orange spectrum

--- led/new_wave.py
+++ led/new_wave.py
@@ -8,6 +8,10 @@ class NewWavePattern(WavePattern):
             point = 1
         elif point < -1:
             point = -1
-        return [int(127 - point*127), 0, int(127 + point*127)]
+        # return [int(127 - point*127), 0, int(127 + point*127)]
         
+        if point > 0:
+            return [int(255 * point), 0, 0]
+        else:
+            return [int(255 * -point), int(85 * -point), 0]
         
