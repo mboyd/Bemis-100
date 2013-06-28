024e865d42d1689f612efb928be5315e21570e92
Robin Deits <robin.deits@gmail.com>
2013-06-28 00:35:38 -0400

    disable preview

--- Web/static/javascripts/bemis100.js
+++ Web/static/javascripts/bemis100.js
@@ -6,41 +6,41 @@ var frameCount;
 var lastTime;
 
 $(document).ready(function() {
-  
+
   canvas = document.getElementById('canvas');
   ctx = canvas.getContext('2d');
   blank();
-  
+
   frameCount = 0;
   lastTime = (new Date).getTime();
-  
-  connect();
+
+  // connect();
 
   updateDevices();
   updateWriters();
-  
+
   $('#play_controls a').click(function(evt) {
     evt.preventDefault();
-    
-    if ($(evt.target).hasClass('pause')) {  
+
+    if ($(evt.target).hasClass('pause')) {
       $.getJSON('/pause', function(result) {
-      
+
       });
-    
+
       $(evt.target).removeClass('pause');
       $(evt.target).addClass('play');
       $(evt.target).html('Play');
-    
-    } else if ($(evt.target).hasClass('play')) { 
+
+    } else if ($(evt.target).hasClass('play')) {
       $.getJSON('/play', function(result) {
-      
+
       });
       $(evt.target).removeClass('play');
       $(evt.target).addClass('pause');
       $(evt.target).html('Pause');
 	  updateQueue();
-    
-    } else if ($(evt.target).hasClass('next')) { 
+
+    } else if ($(evt.target).hasClass('next')) {
       $.getJSON('/next', function(result) {
         updateQueue();
       });
@@ -59,70 +59,70 @@ $(document).ready(function() {
 
   $('#patterns a').click(function(evt) {
     evt.preventDefault();
-    
+
     var p = evt.target.getAttribute('data-pattern');
-    
+
     var track_beat = $('#pattern_config input[name=beat_tracking]').is(':checked');
-    
+
     params = {pattern: p};
     if (track_beat) {
       params.beat = true;
     }
-    
+
     // $('#play_pause').removeClass('pause');
     // $('#play_pause').addClass('play');
     // $('#play_pause').html('Play');
-    
+
     $.getJSON('/add', params, function(result) {
       updateQueue();
     });
   });
-  
+
 });
 
 function connect() {
 	//console.log('connecting')
 
   ws = io.connect('/');
-  
+
   ws.on('connect', function() {
     $('#connection').html('Status: connected');
     updateQueue();
 	// console.log('Opened WS connection')
   });
-  
+
   ws.on('message', function(e) {
     var data = JSON.parse(e);
-    
+
     if (data['status'] == 'ok') {
 		// console.log(data['frame']);
       var frame = data['frame'];
       var pixelWidth = canvas.width / frame.length;
       var height = canvas.height;
-      
+
       for (var i = 0; i < frame.length; i++) {
         ctx.fillStyle = frame[i];
         ctx.fillRect(i*pixelWidth, 0, pixelWidth, height);
       }
-      
+
       if (frameCount == 30) {
         frameCount = 0;
         var t = (new Date).getTime();
         var dt = t - lastTime;
         var fr = 30 / (dt / 1000.0);
         $('#framerate').html('Framerate: ' + fr.toFixed(2) + ' fps');
-      
+
         lastTime = t;
       }
-      
+
       frameCount += 1;
-      
+
     } else if (data['status'] == 'exiting') {
       $('#framerate').html('');
       //ws.close();
     }
   });
-  
+
   ws.on('disconnect', function() {
 	  console.log('Closed WS connection')
     $('#connection').html('Status: disconnected');
@@ -171,17 +171,17 @@ function updateQueue() {
   $.getJSON('/queue', function(result) {
     var queue = result['queue'];
     var qh = '';
-    
+
     if (queue.length == 0) {
       qh = '<li>empty</li>';
     }
-    
+
     for (var i = 0; i < queue.length; i++) {
       var p = queue[i][0];
       var n = queue[i][1];
-      qh += 
-        '<li><img src="/static/patterns/' + p + '">' + 
-        
+      qh +=
+        '<li><img src="/static/patterns/' + p + '">' +
+
         '</li>';
     }
     $('#queue ul').html(qh);
